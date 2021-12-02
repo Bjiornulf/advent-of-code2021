6912f5a244b94c3d45cfa6f129985b2a653d1ae8
Bjiornulf <tpeev01@gmail.com>
2021-12-02 09:57:34 +0100

    improvement of solution. easier to read and more concise

--- day1/day1.go
+++ day1/day1.go
@@ -8,18 +8,14 @@ import (
 func main() {
 	fmt.Println("day1")
 	ints := utils.ReadInts("./input");
-	fmt.Printf("Puzzle1: %v\n", countIncreasing(ints));
-	groupedInts := make([]int, len(ints) - 2);
-	for i := 2; i < len(ints); i++ {
-		groupedInts[i-2] = ints[i-2] + ints[i-1] + ints[i];
-	}
-	fmt.Printf("Puzzle2: %v\n", countIncreasing(groupedInts));
+	fmt.Printf("Puzzle1: %v\n", countIncreasing(ints, 1));
+	fmt.Printf("Puzzle2: %v\n", countIncreasing(ints, 3));
 }
 
-func countIncreasing(values []int) int {
+func countIncreasing(values []int, groupSize int) int {
 	var count int = 0;
-	for i := 1; i < len(values); i++ {
-		if (values[i] - values[i-1] > 0) {
+	for i := groupSize; i < len(values); i++ {
+		if (values[i] - values[i-groupSize] > 0) {
 			count++;
 		}
 	}
