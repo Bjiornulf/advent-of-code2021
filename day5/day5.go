14862267f2a680aad58b94472da7ce3cf6c5429e
Bjiornulf <tpeev01@gmail.com>
2021-12-05 12:18:22 +0100

    go fmt

--- day5/day5.go
+++ day5/day5.go
@@ -18,21 +18,21 @@ func main() {
 
 	/* ----------- Puzzle 1 ---------- */
 	// only keep vertical or horrizontal lines
-	puzzle1Lines := filterLines(lines, func (l line) bool {return (l.x1 == l.x2 || l.y1 == l.y2)})
+	puzzle1Lines := filterLines(lines, func(l line) bool { return (l.x1 == l.x2 || l.y1 == l.y2) })
 	xmax, ymax := maxCoords(puzzle1Lines)
 	puzzle1Grid := initGrid(xmax+1, ymax+1)
 	fillGrid(&puzzle1Grid, puzzle1Lines)
-	fmt.Printf("Puzzle1: %v\n", countMatrix(puzzle1Grid, func (i int) bool {return i>=2}))
+	fmt.Printf("Puzzle1: %v\n", countMatrix(puzzle1Grid, func(i int) bool { return i >= 2 }))
 
 	/* ----------- Puzzle 2 ---------- */
-	puzzle2Lines := filterLines(lines, func (l line) bool {
+	puzzle2Lines := filterLines(lines, func(l line) bool {
 		// only keep vertical, horrizontal or 45 degree lines
-		return (l.x1 == l.x2 || l.y1 == l.y2 || abs(l.x1 - l.x2) == abs(l.y1 - l.y2))
+		return (l.x1 == l.x2 || l.y1 == l.y2 || abs(l.x1-l.x2) == abs(l.y1-l.y2))
 	})
 	xmax2, ymax2 := maxCoords(puzzle2Lines)
-	puzzle2Grid := initGrid(xmax2+1, ymax2+1) 
+	puzzle2Grid := initGrid(xmax2+1, ymax2+1)
 	fillGrid(&puzzle2Grid, puzzle2Lines)
-	fmt.Printf("Puzzle2: %v\n", countMatrix(puzzle2Grid, func (i int) bool {return i>=2}))
+	fmt.Printf("Puzzle2: %v\n", countMatrix(puzzle2Grid, func(i int) bool { return i >= 2 }))
 }
 
 // make an [][]int grid of size width * height
@@ -57,7 +57,11 @@ func fillGrid(grid *[][]int, lines []line) {
 }
 
 func max(a int, b int) int {
-	if a < b { return b } else { return a }
+	if a < b {
+		return b
+	} else {
+		return a
+	}
 }
 
 // return 1 if a > b; 0 if a == b; -1 if a < b
@@ -69,21 +73,22 @@ func intCmp(a int, b int) int {
 	}
 	return 0
 }
+
 // creates an array with the points a line crosses
 func enumPositions(l line) (x []int, y []int) {
-	size := max(abs(l.x1 - l.x2), abs(l.y1 - l.y2))
-	x = make([]int, size + 1)
-	y = make([]int, size + 1)
+	size := max(abs(l.x1-l.x2), abs(l.y1-l.y2))
+	x = make([]int, size+1)
+	y = make([]int, size+1)
 	xIncr, yIncr := intCmp(l.x2, l.x1), intCmp(l.y2, l.y1)
 	for i := 0; i <= size; i++ {
-		x[i] = l.x1 + xIncr * i
-		y[i] = l.y1 + yIncr * i
+		x[i] = l.x1 + xIncr*i
+		y[i] = l.y1 + yIncr*i
 	}
 	return x, y
 }
 
 // counts the number of occurences in a matrix that match the filter function (filter(x) == true)
-func countMatrix(matrix [][]int, filter func (int) bool) int {
+func countMatrix(matrix [][]int, filter func(int) bool) int {
 	var count int
 	for i := range matrix {
 		for j := range matrix[0] {
@@ -103,19 +108,19 @@ func importData(lines []string) []line {
 		start := strings.SplitN(fields[0], ",", 2)
 		end := strings.SplitN(fields[2], ",", 2)
 		x1, err := strconv.Atoi(start[0])
-		if (err != nil) {
+		if err != nil {
 			panic(err)
 		}
 		y1, err := strconv.Atoi(start[1])
-		if (err != nil) {
+		if err != nil {
 			panic(err)
 		}
 		x2, err := strconv.Atoi(end[0])
-		if (err != nil) {
+		if err != nil {
 			panic(err)
 		}
 		y2, err := strconv.Atoi(end[1])
-		if (err != nil) {
+		if err != nil {
 			panic(err)
 		}
 		res[i] = line{x1: x1, y1: y1, x2: x2, y2: y2}
@@ -124,7 +129,7 @@ func importData(lines []string) []line {
 }
 
 // filter an array of lines according to filter function; returns a new array
-func filterLines(lines []line, filter func (line) bool) []line {
+func filterLines(lines []line, filter func(line) bool) []line {
 	res := make([]line, 0)
 	for _, l := range lines {
 		if filter(l) {
