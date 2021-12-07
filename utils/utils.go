3212eeb20723b5794da68e91ecbd79abdf52516b
Bjiornulf <tpeev01@gmail.com>
2021-12-07 06:37:23 +0100

    [MOV]: moving int utils in separate file

--- utils/utils.go
+++ utils/utils.go
@@ -54,30 +54,3 @@ func ReadIntsList(str string, sep string) []int {
 	}
 	return res
 }
-
-func IntMax(a ...int) int {
-	max := a[0]
-	for _, i := range a {
-		if i > max {
-			max = i
-		}
-	}
-	return max
-}
-
-func IntMin(a ...int) int {
-	min := a[0]
-	for _, i := range a {
-		if i < min {
-			min = i
-		}
-	}
-	return min
-}
-
-func IntAbs(a int) int {
-	if a > 0 {
-		return a
-	}
-	return -a
-}
