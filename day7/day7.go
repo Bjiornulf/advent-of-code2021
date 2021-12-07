5f8a31bc59e86ddc57e7e88c87593bf6ead4007a
Bjiornulf <tpeev01@gmail.com>
2021-12-07 09:30:22 +0100

    comments

--- day7/day7.go
+++ day7/day7.go
@@ -9,15 +9,22 @@ func main() {
 	fmt.Println("day7")
 	input := utils.ReadLines("input")
 	crabs := utils.ReadIntsList(input[0], ",")
+	// cost to target when each step costs 1
 	fuelFuction1 := func(c, t int) int {return utils.IntAbs(c-t)}
+	// cost to target when each step increasingly costs 1 more : 1, 2, 3...
 	fuelFuction2 := func(c, t int) int {
 		n := utils.IntAbs(c-t)
 		return n*(n+1) / 2
 	}
+
+	/* ---------- Puzzle 1 ---------- */
 	fmt.Println("Puzzle1:", findMinFuel(crabs, fuelFuction1))
+
+	/* ---------- Puzzle 2 ---------- */
 	fmt.Println("Puzzle2:", findMinFuel(crabs, fuelFuction2))
 }
 
+// return the miniumum total fuel required so that every crab ends on the same position
 func findMinFuel(crabs []int, fuelFuction func(int, int) int) int {
 	xmax := utils.IntMax(crabs...)
 	fuels := make([]int, xmax)
@@ -27,6 +34,7 @@ func findMinFuel(crabs []int, fuelFuction func(int, int) int) int {
 	return utils.IntMin(fuels...)
 }
 
+// calculate the total fuel cost to move every crab to the target
 func calcTotalFuelConsumption(crabs []int, target int, fuelFuction func(int, int) int) int {
 	fuelSum := 0
 	for _, crab := range crabs {
