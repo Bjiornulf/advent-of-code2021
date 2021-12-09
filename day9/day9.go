7171003bec07ca647e7e72002939254f10432866
Bjiornulf <tpeev01@gmail.com>
2021-12-09 16:06:10 +0100

    [ADD]: Solution for day 9

---
+++ day9/day9.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"aoc2021/utils"
+	"fmt"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	input := utils.ReadLines("input")
+	fmt.Println("day9")
+
+	/* ---------- Puzzle 1 ---------- */
+	fmt.Println("Puzzle1:", puzzle1(input))
+
+	/* ---------- Puzzle 2 ---------- */
+	fmt.Println("Puzzle2:", puzzle2(input))
+}
+
+func puzzle1(input []string) int {
+	lows, _ := lowLevels(importData(input))
+	sum := 0
+	for _, l := range lows {
+		sum += l + 1
+	}
+	return sum
+}
+
+func puzzle2(input []string) int {
+	bassins := bassinsSizes(importData(input))
+	sort.Ints(bassins)
+	end := len(bassins) - 1
+	return bassins[end] * bassins[end-1] * bassins[end-2]
+}
+
+/* ---------- Structures and their methods ---------- */
+type Coord struct {
+	i, j int
+}
+
+type CoordMap map[Coord]bool
+
+func (cm *CoordMap) Contains(c Coord) bool {
+	v, ok := (*cm)[c]
+	return ok && v
+}
+
+func (cm *CoordMap) Add(c Coord) {
+	(*cm)[c] = true
+}
+
+/* ---------- Functions ---------- */
+// import the data for the day
+func importData(input []string) [][]int {
+	res := make([][]int, len(input))
+	for i := range input {
+		res[i] = make([]int, len(input[i]))
+		for j := range input[i] {
+			level, _ := strconv.Atoi(string(input[i][j]))
+			res[i][j] = level
+		}
+	}
+	return res
+}
+
+// return all points that are low level (their level and their coordinates)
+func lowLevels(mat [][]int) ([]int, []Coord) {
+	res := make([]int, 0)
+	coords := make([]Coord, 0)
+	for i := range mat {
+		for j := range mat[i] {
+			isLow := true
+			if i == 0 {
+				isLow = isLow && (mat[i+1][j] > mat[i][j])
+			} else if i == len(mat)-1 {
+				isLow = isLow && (mat[i-1][j] > mat[i][j])
+			} else {
+				isLow = isLow && (mat[i-1][j] > mat[i][j]) && (mat[i+1][j] > mat[i][j])
+			}
+			if j == 0 {
+				isLow = isLow && (mat[i][j+1] > mat[i][j])
+			} else if j == len(mat[i])-1 {
+				isLow = isLow && (mat[i][j-1] > mat[i][j])
+			} else {
+				isLow = isLow && (mat[i][j-1] > mat[i][j]) && (mat[i][j+1] > mat[i][j])
+			}
+			if isLow {
+				res = append(res, mat[i][j])
+				coords = append(coords, Coord{i: i, j: j})
+			}
+		}
+	}
+	return res, coords
+}
+
+func NewCoordMap() *CoordMap {
+	c := new(CoordMap)
+	*c = make(map[Coord]bool)
+	return c
+}
+
+func bassinsSizes(mat [][]int) []int {
+	lows, coords := lowLevels(mat)
+	res := make([]int, 0)
+	for i := range lows {
+		size := 0
+		explored := NewCoordMap()
+		recExplore(mat, coords[i], explored, &size)
+		res = append(res, size)
+	}
+	return res
+}
+
+func recExplore(mat [][]int, start Coord, explored *CoordMap, size *int) {
+	i, j := start.i, start.j
+	if explored.Contains(start) {
+		return // this has been explored
+	}
+	if i < 0 || i >= len(mat) || j < 0 || j >= len(mat[i]) {
+		return // this is not explorable (outisde of matrix bounds)
+	}
+	if mat[i][j] == 9 {
+		return // 9 is never in a basin
+	}
+
+	// Every point that can be reached by exploring the neighbours only is in the same bassin
+
+	/* ---------- Recursive exploration of the neightbours ---------- */
+	explored.Add(start)
+	(*size)++
+	recExplore(mat, Coord{i: i - 1, j: j}, explored, size)
+	recExplore(mat, Coord{i: i + 1, j: j}, explored, size)
+	recExplore(mat, Coord{i: i, j: j + 1}, explored, size)
+	recExplore(mat, Coord{i: i, j: j - 1}, explored, size)
+}
