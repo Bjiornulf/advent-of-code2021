03dbc916680476f66ab037a1aecfdbe26be487b2
Bjiornulf <tpeev01@gmail.com>
2021-12-09 19:14:01 +0100

    tests on sample data for day8

---
+++ day8/day8_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+)
+
+var testInput []string = []string{
+	"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+	"edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+	"fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+	"fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+	"aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+	"fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+	"dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+	"bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+	"egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+	"gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce",
+}
+
+func TestPuzzle1(t *testing.T) {
+	res := puzzle1(testInput)
+	const expected = 26
+	if (expected != res) {
+		t.Errorf("Puzzle1: expected %v but got %v\n", expected, res)
+	}
+}
+
+func TestPuzzle2(t *testing.T) {
+	res := puzzle2(testInput)
+	const expected = 61229
+	if (expected != res) {
+		t.Errorf("Puzzle2: expected %v but got %v\n", expected, res)
+	}
+}
