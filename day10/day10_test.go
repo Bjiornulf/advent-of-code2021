912dcb170d0e8b4cf0f8cc9696cd590a144d09ab
Bjiornulf <tpeev01@gmail.com>
2021-12-10 14:26:14 +0100

    benchmark day10

--- day10/day10_test.go
+++ day10/day10_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"aoc2021/utils"
 	"testing"
 )
 
@@ -17,6 +18,8 @@ var testInput = []string{
 	"<{([{{}}[<[[[<>{}]]]>[]]",
 }
 
+var input = utils.ReadLines("input")
+
 func TestPuzzle1(t *testing.T) {
 	res := puzzle1(testInput)
 	expected := 26397
@@ -30,5 +33,17 @@ func TestPuzzle2(t *testing.T) {
 	expected := 288957
 	if (res != expected) {
 		t.Errorf("Puzzle1: expected %v but got %v\n", expected, res)
+	}
+}
+
+func BenchmarkPuzzle1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		puzzle1(input)
+	}
+}
+
+func BenchmarkPuzzle2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		puzzle2(input)
 	}
 }
