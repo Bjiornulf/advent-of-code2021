6ba8b9750cca4dd7de7381663ed9db31716a6d9c
Bjiornulf <tpeev01@gmail.com>
2021-12-15 11:36:19 +0100

    added test for day 15

--- day15/day15_test.go
+++ day15/day15_test.go
@@ -1,11 +1,39 @@
 package main
 
 import (
-	"testing"
 	"aoc2021/utils"
+	"testing"
 )
 
 var input = utils.ReadLines("input")
+var testInput = []string{
+	"1163751742",
+	"1381373672",
+	"2136511328",
+	"3694931569",
+	"7463417111",
+	"1319128137",
+	"1359912421",
+	"3125421639",
+	"1293138521",
+	"2311944581",
+}
+
+func TestPuzzle1(t *testing.T) {
+	res := puzzle1(testInput)
+	expected := 40
+	if res != expected {
+		t.Errorf("Puzzle1: exptected %v but got %v\n", expected, res)
+	}
+}
+
+func TestPuzzle2(t *testing.T) {
+	res := puzzle2(testInput)
+	expected := 315
+	if res != expected {
+		t.Errorf("Puzzle2: exptected %v but got %v\n", expected, res)
+	}
+}
 
 func BenchmarkPuzzle1(b *testing.B) {
 	for i := 0; i < b.N; i++ {
