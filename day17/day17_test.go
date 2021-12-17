398ae28d5af453fc3807ae0be0a65e3a6177f063
Bjiornulf <tpeev01@gmail.com>
2021-12-17 14:09:29 +0100

    more tests and benchmarks

--- day17/day17_test.go
+++ day17/day17_test.go
@@ -3,14 +3,47 @@ package main
 import (
 	"aoc2021/utils"
 	"fmt"
+	"math/rand"
 	"strconv"
 	"strings"
 	"testing"
 	"unicode"
-	"math/rand"
 )
 
+const testInput = "target area: x=20..30, y=-10..-5"
 const input = "target area: x=179..201, y=-109..-63"
+
+func TestPuzzle1_1(t *testing.T) {
+	res := puzzle1([]string{testInput})
+	expected := 45
+	if expected != res {
+		t.Errorf("Puzzle 1 - test input: expected %v but got %v\n", expected, res)
+	}
+}
+
+func TestPuzzle1_2(t *testing.T) {
+	res := puzzle1([]string{input})
+	expected := 5886
+	if expected != res {
+		t.Errorf("Puzzle 1 - my input: expected %v but got %v\n", expected, res)
+	}
+}
+
+func TestPuzzle2_1(t *testing.T) {
+	res := puzzle2([]string{testInput})
+	expected := 112
+	if expected != res {
+		t.Errorf("Puzzle 2 - test input: expected %v but got %v\n", expected, res)
+	}
+}
+
+func TestPuzzle2_2(t *testing.T) {
+	res := puzzle2([]string{input})
+	expected := 1806
+	if expected != res {
+		t.Errorf("Puzzle 2 - my input: expected %v but got %v\n", expected, res)
+	}
+}
 
 func BenchmarkSscanf(b *testing.B) {
 	for i := 0; i < b.N; i++ {
@@ -22,7 +55,7 @@ func BenchmarkSscanf(b *testing.B) {
 func BenchmarkFields(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		var x1, x2, y1, y2 int
-		vals := strings.FieldsFunc(input, func (c rune) bool {
+		vals := strings.FieldsFunc(input, func(c rune) bool {
 			return !unicode.IsNumber(c) && c != '-'
 		})
 		x1, _ = strconv.Atoi(vals[0])
@@ -33,14 +66,14 @@ func BenchmarkFields(b *testing.B) {
 	}
 }
 
-func BenchmarkUtilsMin (b *testing.B) {
+func BenchmarkUtilsMin(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		utils.IntMin(rand.Int(), rand.Int())
 	}
 }
 
-func BenchmarkMin (b *testing.B) {
-	min := func (a, b int) int {
+func BenchmarkMin(b *testing.B) {
+	min := func(a, b int) int {
 		if a < b {
 			return a
 		}
@@ -48,5 +81,17 @@ func BenchmarkMin (b *testing.B) {
 	}
 	for i := 0; i < b.N; i++ {
 		min(rand.Int(), rand.Int())
+	}
+}
+
+func BenchmarkPuzzle1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		puzzle1([]string{input})
+	}
+}
+
+func BenchmarkPuzzle2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		puzzle2([]string{input})
 	}
 }
